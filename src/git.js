c11ee73435f324bdceaeff09ec5aeb18a6f9050e
Urip Subekti <imanuel.subekti@efishery.com>
2022-10-26 08:56:59 +0700

    feat: add getTags and getCommitMessagesAfterLastTag

--- src/git.js
+++ src/git.js
@@ -1,8 +1,31 @@
 const { execAsync } = require("./exec");
 
 const getTags = async () => {
-  const res = await execAsync("git tag");
-  console.log({ res });
+  const stdout = await execAsync("git tag");
+  const tags = (stdout || "")
+    .split("\n")
+    .filter((x) => x)
+    .reverse();
+  return tags;
 };
 
-getTags()
+const getCommitMessagesAfterLastTag = async (tag = null) => {
+  const tags = await getTags();
+  const lastTags = tags?.[0];
+  let stdout;
+  if (lastTags) {
+    stdout = await execAsync(
+      `git log ${lastTags}..HEAD --no-merges --oneline --pretty=tformat:"%s"`
+    );
+  } else {
+    stdout = await execAsync(
+      `git log --no-merges --oneline --pretty=tformat:"%s"`
+    );
+  }
+
+  const commitMessages = (stdout || "").split("\n").filter((x) => x);
+  return commitMessages;
+};
+
+// getCommitMessagesAfterLastTag();
+module.exports = { getTags, getCommitMessagesAfterLastTag };
